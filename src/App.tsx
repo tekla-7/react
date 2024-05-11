523df63ee9ae069322698c2241a87200f6f96aa8
tekla kvekvetsia <kvekveciatekla@gmail.com>
2024-05-11 16:00:32 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Message from "./Message"
+
+function App(){
+  // return <div><Message></Message></div>
+  return <div><Message /></div>
+}
+export default App
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+
+function Message(){
+    //jsx:JAVASCRIPT 
+  return <h1>Hello wORD</h1>
+}
+export default Message;
