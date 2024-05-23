d739505bb5a3db735bf3c824e6e5b141ef8e83f6
tekla kvekvetsia <kvekveciatekla@gmail.com>
2024-05-23 16:12:11 +0400

    commit prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import reactImg from './assets/react.svg'
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import componentsImg from './assets/components.png'
+import {CORE_CONCEPTS} from './data.js';
 
 function genRandomInt(max:number) {
   return Math.floor(Math.random() * (max + 1));
@@ -8,7 +11,7 @@ function Header() {
   const description=reactDescriptions[genRandomInt(2)]
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are going to build!
@@ -16,12 +19,38 @@ function Header() {
     </header>
   );
 }
-
+function CoreConcept(props){
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+           {...CORE_CONCEPTS[2]} />
+            <CoreConcept 
+            {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcept 
+             {...CORE_CONCEPTS[3]}
+            />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
